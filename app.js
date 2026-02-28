c1e1ba9a2473924fffb9595541da9eaab2a71d67
hasan <kaligrafihuseyin05@gmail.com>
2026-02-28 17:46:08 +0300

    workspace created

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const fn = function () {
-    console.log('Function working');
-}
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+//TEMPLATE ENGİNE
+app.set("view engine", "ejs");
+
+
+// MiddlewareS
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+    //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
+});
+app.get('/about', (req, res) => { 
+    res.render('about');
+});
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+})
+
